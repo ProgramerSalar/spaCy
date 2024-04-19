88d50c73dc9aceb4fee988bfb9a6638b4585da21
ProgramerSalar <manishkumar60708090@gmail.com>
2024-04-19 18:10:24 +0530

    import the data

--- 01_customazing_spacy.py
+++ 01_customazing_spacy.py
@@ -1,7 +1,18 @@
 ## Name Entity Recognition :- 
 
 import spacy 
-
+import json 
+from spacy.lang.en import English
+from spacy.pipeline import EntityRuler
         
+
+# import the data 
+def load_data(file):
+    # open the file and read encodeing is utf-8
+    with open(file, 'r', encoding="utf-8") as f:
+        # format data is json 
+        data = json.load(f)
+    return (data)
+
 
 
